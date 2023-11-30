25bfcef11a9da3532ab28c3da587e6ac362da63b
dkijania <dariusz@o1labs.org>
2023-11-30 22:36:32 +0100

    fix cache

--- buildkite/scripts/upload-deb.sh
+++ buildkite/scripts/upload-deb.sh
@@ -4,6 +4,6 @@ set -eo pipefail
 
 DEBS='_build/mina-*.deb'
 
-for entry in "$DEBS"/*
-do
-  ./buildkite/scripts/cache-through.sh $entry
+for entry in $DEBS; do
+  ./buildkite/scripts/cache-through.sh $entry "echo \" cannot upload file $entry \" "
+done
