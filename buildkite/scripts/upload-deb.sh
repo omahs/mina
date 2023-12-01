fa540270bb63688a1ee3d086c028c95acf683bc6
dkijania <dariusz@o1labs.org>
2023-12-01 21:03:01 +0100

    go inside _build before caching

--- buildkite/scripts/upload-deb.sh
+++ buildkite/scripts/upload-deb.sh
@@ -2,11 +2,15 @@
 
 set -eo pipefail
 
-DEBS='_build/mina-*.deb'
+DEBS='mina-*.deb'
 mkdir ${BUILDKITE_BUILD_ID}
+
+cd _build
 
 for entry in $DEBS; do
   cp $entry ${BUILDKITE_BUILD_ID}/$entry
    
   ./buildkite/scripts/cache-through.sh ${BUILDKITE_BUILD_ID}/$entry "echo \" cannot upload file $entry \" "
 done
+
+cd ../
